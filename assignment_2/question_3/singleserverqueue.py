8a6259d98192a8e015251cff3a7512f8c7694be1
threexc <tvgamblin@gmail.com>
2018-02-11 21:41:04 -0500

    improved histograms; changed sample images

--- assignment_2/question_3/singleserverqueue.py
+++ assignment_2/question_3/singleserverqueue.py
@@ -13,6 +13,7 @@ def queue_hist(filename):
     service2_list = []
     bin_count = 0
 
+
     with open(filename) as csvfile:
         readCSV = csv.reader(csvfile, delimiter=',')
         for row in readCSV:
@@ -34,18 +35,24 @@ def queue_hist(filename):
         service2_rate = 1 / average_service2
     bin_count = int(np.floor(1.66*(count**(1/3))))
 
+    service1_max = max(service1_list)
+    service2_max = max(service2_list)
+
+    hist1_range = np.arange(0, service1_max, 10)
+    hist2_range = np.arange(0, service2_max, 10)
     inter_hist = np.histogram(interarrival_list, bins=bin_count)
-    service1_hist = np.histogram(service1_list, bins=bin_count)
-    service2_hist = np.histogram(service2_list, bins=bin_count)
+    service1_hist = np.histogram(service1_list, bins=hist1_range)
+    service2_hist = np.histogram(service2_list, bins=hist2_range)
+
 
     plt.hist(interarrival_list, bins=bin_count)
     plt.title("Interarrival Histogram")
     plt.savefig("interarrival.jpg")
 
-    plt.hist(service1_hist, bins=bin_count)
+    plt.hist(service1_hist, bins=hist1_range)
     plt.title("Service Time 1 Histogram")
     plt.savefig("service1.jpg")
 
-    plt.hist(service2_hist, bins=bin_count)
+    plt.hist(service2_hist, bins=hist2_range)
     plt.title("Service Time 2 Histogram")
     plt.savefig("service2.jpg")
